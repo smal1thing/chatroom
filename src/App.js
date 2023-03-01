e51997d62b21363a05c20df505799ef602855526
蔡致行 <caizhihang@caizhixingdeMacBook-Pro.local>
2023-02-28 23:43:43 -0800

    拉起微信支付

--- src/App.js
+++ src/App.js
@@ -4,8 +4,38 @@ import { useState, useEffect, useRef } from 'react';
 import { Input, Button, message, Modal } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
+import JSEncrypt from 'jsencrypt'
+import CryptoJS from "crypto-js";
 import md5 from './md5';
 
+const PRIVATE_KEY = `-----BEGIN PRIVATE KEY-----
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDiIV/USRpWzzb+
+6f92AiZSApE6Xka1iJztOfwt9mYQ88mweFRQjurRLw+6aP9yFKD1GmZhoEH/ToJ7
+n0N3S2Xu6cT70piHQRGAVP50sxTYEfIm7y1Qro3LKMdWlkbgeyCPXojUkT/z9nGU
+zJqAraoHX4SQZhqeGuAfHNMj2evIKJ/GoBSTUXZxfI2kuHg9Y6W7kt7VXTa6Xvw3
+snZytnsfx6Dpva/ouzBMfHWfoauOLdG53vzFAlLEbjxRuyQAQhMclGHUnGoxqjwP
+QOIGrngkgyIkp7MRSLIGp3Vo5jaIcHBTx8hx9n4SWFO/bBvZYo0xK4nWdPC8q4JL
+q+8q9YAHAgMBAAECggEBANy6HRNXRBmGbLVI7a5gDM2yadYinjymnB9HoWuv/xL/
+FFloK0zzJCyKFn0r7mSJ1E9LtLIIv0MZfG51GGLCuz4I+9mfSHmFvzKYRETfZTI/
+2jG892uw5wFuzZ0sVQTbyv2HFmL/YQCfB2FqkkmWusg1qW1V7Rd1Pl3AQizVk7ws
+bS7gRmXF2OGBIu/8mLzAfdNnI8j9uTjDcC49M9KdkPkypXN7yiByjU+angYq+giG
+/puCkMEH9d44eFN4i0iA8BnKMEwxugnKilL/wynbgw0JHnutwvPF3oeNj+J6LVaw
+wU4Gfoijov2ni5MP4fH6tG7dIlhqjdNZsmeMQ6pkYTECgYEA+jN8zuXG6tSXaRR3
+ZgjeLk9b6iUADErNggpmZ62KWx/gVI2OActSW6JIB5SHOSXbx5I7qTfQAjjN+BaY
+pvJJYOsZdU+Bk6JeaUAcsXGCi9L2HhCmo+t19UNe1FVV105T4YZEMKHRTRvjKd4f
+t8LS5j6RzfI0l9Rprn3EfKDMpX8CgYEA518Rf8CETc2vZIgwOoSd4hyJ6J1r2wag
+RrPx0AO2DZqu499K7hdGp/ztM/xlikW1a4+aurte4A7Gr41Gxh+dVQ5hDUTtewtR
+bPDaMAYi1NRo0kraHXCpbOixOhLW4XNWTM1ALypE+aLxEiGBc3Oe5sIf0WAJLWVb
+EVnpsQueOXkCgYEAsZ9n1YIuq3vtWb4b3aYiBYJ1YE6QMCnSp6U2ehgrhvGkUqKD
+1CZB+6fDtw3syddkpdPc4w8qbslg/+UazjpneZSt6Chfdy3oFJRdSmOpKBbGfyWX
+B/wbK9l+MwO6AzYqOosVUekvK6zGomx82/pFuwtke26dg8RamnPS0B9f6YUCgYBB
+PsTfgrmMezE3p1P6XIVtSuD08NeGZ0LxTTMmlrVS9sjUx5YIuBWbr65wV3+G04uK
+bm+Sst3ZTzFmNe+8VRP39VsW89YIObPXhb/xhPlzjQaWLrd9T9TnOmMn3kIsR2sR
+s1ujMUdMIk/a7gnkNbmclyeD0pIj9A4PQYyt+Xm/+QKBgQCar5TCarOV93AemxPo
+1LcxAMBe6AKTkYtvzbAIaCgnNKV3hwq74dlO/m0In0mPumOk0/pjgXyr4jEoFbwg
+bDjpcVytgh4M9nE2hImmnEOWF1Mqnug09QRg1qhqLVpTjRB+fLNwxq0S84oz4wnQ
+jTFseK2f9Iwvn6t+AlDrB9rNyQ==
+-----END PRIVATE KEY-----`
 const ROBOT_AVATAR = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg2.doubanio.com%2Fview%2Frichtext%2Flarge%2Fpublic%2Fp53389253.jpg&refer=http%3A%2F%2Fimg2.doubanio.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1679757641&t=a44588a81d7efe7dc16416d4b89059eb";
 const USER_AVATAR = "https://ts1.cn.mm.bing.net/th/id/R-C.0a1f275d1d27d96f996ae5af9cc838eb?rik=ifvouAHyyFj8cg&riu=http%3a%2f%2fwww.yulumi.cn%2fgl%2fuploads%2fallimg%2f201204%2f3-20120415332MI.jpg&ehk=FWvR8lLqblGnk%2bLTcv7CSk8AlAh1U5CdHJDAoHY45rc%3d&risl=&pid=ImgRaw&r=0";
 const baseUrl = "https://platypus.yazuishoudalu.com/"
@@ -178,7 +208,11 @@ function App() {
 
   return (
     <div className="chat-room">
-      {userId !== "" && <div className='user-id'>userid: {userId}</div>}
+      {userId !== "" && <div className='user-id' onClick={() => {
+        setModalVisible(true);
+        setRechargeAmount();
+        setInvitationCode();
+      }}>userid: {userId}</div>}
       <div className='message-part'>
         {
           messageList.map(item =>
@@ -188,14 +222,14 @@ function App() {
       </div>
       <div className='typing-part'>
         <div className="">
-          <Button 
-            className='charge-button' 
+          {/* <Button
+            className='charge-button'
             onClick={() => {
-              setModalVisible(true); 
+              setModalVisible(true);
               setRechargeAmount();
               setInvitationCode();
             }}
-          >充值</Button>
+          >充值</Button> */}
           {/* <Button className='charge-button' disabled>历史对话</Button> */}
           {/* <div className='balance'> 还可以提问{balance}次</div> */}
         </div>
@@ -209,26 +243,26 @@ function App() {
           <Button className='sent-button' type='primary' onClick={() => sendMessage(inputText)}>发送</Button>
         </div>
       </div>
-      <Modal 
-        title="充值" 
-        visible={modalVisible} 
-        onOk={()=> {
+      <Modal
+        title="充值"
+        visible={modalVisible}
+        onOk={() => {
           if (rechargeAmount) {
             invokePaymentWindow(rechargeAmount, invitationCode);
           } else {
             message.info('请至少选择一个金额');
           }
         }}
-        onCancel={() => {setModalVisible(false); setRechargeAmount();}}
+        onCancel={() => { setModalVisible(false); setRechargeAmount(); }}
         width="60%"
         maskClosable={false}
         closable={false}
         cancelText="取消"
         okText="确认"
       >
-        <Button type={rechargeAmount === 3 ? "primary" : null} onClick={()=> setRechargeAmount(3)}>3元</Button>
-        <Button type={rechargeAmount === 18 ? "primary" : null} onClick={()=> setRechargeAmount(18)} style={{"marginLeft": '5px'}}>18元</Button>
-        <Input value={invitationCode} onChange={(v) => setInvitationCode(v.target.value)} placeholder="请输入邀请码（如有）" style={{"marginTop": '5px'}}></Input>
+        <Button type={rechargeAmount === 3 ? "primary" : null} onClick={() => setRechargeAmount(3)}>3元</Button>
+        <Button type={rechargeAmount === 18 ? "primary" : null} onClick={() => setRechargeAmount(18)} style={{ "marginLeft": '5px' }}>18元</Button>
+        <Input value={invitationCode} onChange={(v) => setInvitationCode(v.target.value)} placeholder="请输入邀请码（如有）" style={{ "marginTop": '5px' }}></Input>
       </Modal>
     </div>
   );
@@ -278,26 +312,26 @@ const handlePay = (prepay_id) => {
 const onBridgeReady = (prepay_id) => {
   message.info("onBridgeReady");
   const appId = "wxfc9591f30d5e5b0b";              //公众号ID，由商户传入  
-  const timeStamp = parseInt(+new Date()/1000);   //时间戳，自1970年以来的秒数  
-  const nonceStr = Math.random().toString(36).slice(-10) + Math.random().toString(36).slice(-10); 
+  const timeStamp = parseInt(+new Date() / 1000);   //时间戳，自1970年以来的秒数  
+  const nonceStr = Math.random().toString(36).slice(-10) + Math.random().toString(36).slice(-10);
   const packageString = "prepay_id=" + prepay_id;
-  const stringA = `appid=${appId}&timeStamp=${timeStamp}&package=${packageString}&nonce_str=${nonceStr}&signType=MD5`;
-  const stringSignTemp = stringA + "&key=b330d6c6d9f1e0199bddd9e192dad56a"; 
-  const paySign = md5.hexMD5(stringSignTemp);
-  console.log(stringSignTemp, paySign)
+  const sign = `${appId}\n${timeStamp}\n${nonceStr}\n${packageString}\n`
+  let encryptor = new JSEncrypt();
+  encryptor.setPrivateKey(PRIVATE_KEY);
+  const paySign = encryptor.sign(sign, CryptoJS.SHA256, "sha256");
   window.WeixinJSBridge.invoke(
     "getBrandWCPayRequest",
     {
-      "appId": appId,       
-      "timeStamp": timeStamp,        
+      "appId": appId,
+      "timeStamp": timeStamp.toString(),
       "nonceStr": nonceStr,      //随机串     
       "package": packageString,
-      "signType": "MD5",     //微信签名方式：     
+      "signType": "RSA",     //微信签名方式：     
       "paySign": paySign //微信签名 
     },
     function (res) {
       if (res.err_msg === "get_brand_wcpay_request:ok") {
-
+        message.info("ok");
       } else {
         message.info("fail");
       }
